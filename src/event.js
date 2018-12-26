6ea7e603e984f5aacfae72698e2a0c8b88b60165
Joshua <joshstockin@gmail.com>
2018-12-25 21:54:43 -0600

    Update event.js

--- src/event.js
+++ src/event.js
@@ -3,14 +3,15 @@ const statement = require("./statement.js");
 class Event {
 	constructor() {
 		this.statements = [];
-		this.push(arguments);
+		this.push(arguments[0], arguments[1], arguments[2]);
 		return this;
 	}
 
 	push() {
 		if (arguments.length == 1) {
-			if (arguments[0].statements) this.statements.push(arguments[0].statements); // object is an Event
-			else this.statements.push(arguments[0]); // object is a Statement
+			if (arguments[0] instanceof Event) this.statements.push(arguments[0].statements);
+			else if (arguments[0] instanceof statement) this.statements.push(arguments[0]);
+			else throw new TypeError("Expected an Event or Statement");
 		}
 		else if (arguments.length == 3) this.statements.push(new statement(arguments[0], arguments[1], arguments[2]));
 		else throw new Error("Expected 1 or 3 arguments");
