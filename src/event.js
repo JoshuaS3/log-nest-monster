4e1388a1c79d51d8a0380cb1cb9f5012514f301e
Joshua <joshstockin@gmail.com>
2018-12-25 21:20:04 -0600

    Update event.js

--- src/event.js
+++ src/event.js
@@ -2,16 +2,9 @@ const statement = require("./statement.js");
 
 class Event {
 	constructor() {
-		if (arguments.length == 0) this.statements = [];
-		else if (arguments.length == 1) {
-			if (!arguments[0].timestamp && !arguments[0].statements) throw new Error("Expected an Event or Statement object");
-			this.statements = [arguments[0]];
-		} else if (arguments.length == 3) {
-			if (typeof arguments[0] != "string" || typeof arguments[1] != "string" || typeof arguments[2] != "string") {
-				throw new Error("Expected 3 strings for Statement creation")
-			};
-			this.statements = [new statement(arguments[0], arguments[1], arguments[2])];
-		}
+		this.statements = [];
+		this.push(arguments);
+		return this;
 	}
 
 	push() {
@@ -19,7 +12,8 @@ class Event {
 			if (arguments[0].statements) this.statements.push(arguments[0].statements); // object is an Event
 			else this.statements.push(arguments[0]); // object is a Statement
 		}
-		if (arguments.length == 3) this.statements.push(new statement(arguments[0], arguments[1], arguments[2]));
+		else if (arguments.length == 3) this.statements.push(new statement(arguments[0], arguments[1], arguments[2]));
+		else throw new Error("Expected 1 or 3 arguments");
 		return this;
 	}
 }
