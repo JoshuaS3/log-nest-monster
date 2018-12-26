9e7f4df608d58d7f48310d6b8237b708a2962606
Joshua <joshstockin@gmail.com>
2018-12-25 21:14:30 -0600

    Update event.js

--- src/event.js
+++ src/event.js
@@ -2,16 +2,23 @@ const statement = require("./statement.js");
 
 class Event {
 	constructor() {
-		if (arguments.length == 0) this.statementsVar = [];
-		if (arguments.length == 1) this.statementsVar = [arguments[0]];
-		if (arguments.length == 3) this.statementsVar = [new statement(arguments[0], arguments[1], arguments[2])];
+		if (arguments.length == 0) this.statements = [];
+		else if (arguments.length == 1) {
+			if (!arguments[0].timestamp && !arguments[0].statements) throw new Error("Expected an Event or Statement object");
+			this.statements = [arguments[0]];
+		} else if (arguments.length == 3) {
+			if (typeof arguments[0] != "string" || typeof arguments[1] != "string" || typeof arguments[2] != "string") {
+				throw new Error("Expected 3 strings for Statement creation")
+			};
+			this.statements = [new statement(arguments[0], arguments[1], arguments[2])];
+		}
 	}
 
-	get statements() {return this.statementsVar}
-	set statements(statements) {this.statementsVar = statements}
-
 	push() {
-		if (arguments.length == 1) this.statements.push(arguments[0].statements);
+		if (arguments.length == 1) {
+			if (arguments[0].statements) this.statements.push(arguments[0].statements); // object is an Event
+			else this.statements.push(arguments[0]); // object is a Statement
+		}
 		if (arguments.length == 3) this.statements.push(new statement(arguments[0], arguments[1], arguments[2]));
 		return this;
 	}
