23d6cc02fc9050bdaad662f51d14d88c2f357448
Joshua <joshstockin@gmail.com>
2018-12-25 21:14:22 -0600

    Update statement.js

--- src/statement.js
+++ src/statement.js
@@ -1,21 +1,11 @@
 class Statement {
 	constructor(verbosity, tag, message) {
-		this.timestampVar = new Date();
-		this.verbosityVar = (verbosity != null && typeof verbosity == "string") ? verbosity : "INFO";
-		this.tagVar = (tag != null && typeof tag == "string") ? tag : "TAG";
-		this.messageVar = (message != null && typeof verbosity == "string") ? message : "";
+		this.timestamp = new Date();
+		this.verbosity = (verbosity != null && typeof verbosity == "string") ? verbosity : "INFO";
+		this.tag = (tag != null && typeof tag == "string") ? tag : "TAG";
+		this.message = (message != null && typeof verbosity == "string") ? message : "";
 		return this;
 	}
-
-	get timestamp() {return this.timestampVar}
-	get verbosity() {return this.verbosityVar}
-	get tag() {return this.tagVar}
-	get message() {return this.messageVar}
-
-	set timestamp(timestamp) {this.timestampVar = timestamp}
-	set verbosity(verbosity) {this.verbosityVar = verbosity}
-	set tag(tag) {this.tagVar = tag}
-	set message(message) {this.messageVar = message}
 }
 
 module.exports = Statement;
