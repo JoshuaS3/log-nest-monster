4d95d0dfdcb7b97dc1b4645ee9c575fc9daca737
Josh Stockin <joshstockin@gmail.com>
2019-11-17 17:40:18 -0600

    Copyright note on lognestmonster.h

--- src/c/lognestmonster.h
+++ src/c/lognestmonster.h
@@ -1,10 +1,30 @@
-// Copyright (c) 2019 Joshua 'joshuas3' Stockin
+// lognestmonster Copyright (c) 2019 Joshua 'joshuas3' Stockin
 // lognestmonster.h
 // C header file for implementation of the lognestmonster logging library
+
+// <https://github.com/JoshuaS3/lognestmonster/>.
+
+
+// This file is part of lognestmonster.
+
+// lognestmonster is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// lognestmonster is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with lognestmonster. If not, see <https://www.gnu.org/licenses/>.
 
 #ifndef __LOGNESTMONSTER__
 #define __LOGNESTMONSTER__ 1
 
-// implementation
+enum lnmVerbosityLevels {lnmInfo, lnmDebug, lnmVerbose, lnmVeryVerbose, lnmWarning, lnmError};
+
+
 
 #endif
