07b6381ae31ed3f3655b87e18339357e6cafb303
Josh Stockin <joshstockin@gmail.com>
2019-11-17 17:15:52 -0600

    Create lognestmonster.h C implementation

---
+++ src/c/lognestmonster.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2019 Joshua 'joshuas3' Stockin
+// lognestmonster.h
+// C header file for implementation of the lognestmonster logging library
+
+#ifndef __LOGNESTMONSTER__
+#define __LOGNESTMONSTER__ 1
+
+// implementation
+
+#endif
