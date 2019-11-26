03180fa975300dd14e833e0c363f76c2e6059fff
Josh Stockin <joshstockin@gmail.com>
2019-11-25 18:57:39 -0600

    Update test

--- tests/main.c
+++ tests/main.c
@@ -23,20 +23,56 @@
 #include <stdio.h>
 #include "lognestmonster.h"
 
+static char * queueName = "master";
+static char * queuePath = "~/path";
+
 int main(void) {
+	long t1 = lnm_getus();
+
 	printf("lognestmonster C test main()\n");
-	printf("enums:\n");
-	printf("\tInfo: %d\n\tDebug: %d\n\tVerbose: %d\n\tVeryVerbose: %d\n\tWarning: %d\n\tError: %d\n", lnmInfo, lnmDebug, lnmVerbose, lnmVeryVerbose, lnmWarning, lnmError);
+	printf("============================\n");
+	printf("\n\n");
 
-	lnmItem event = lnmEvent();
-	lnmEventPushS(event, lnmInfo, "INVOKER/TAG", "Log statement description/message");
-	lnmEventPushS(event, lnmDebug, "INVOKER/TAG", "Log statement description/message");
-	lnmEventPushS(event, lnmVerbose, "INVOKER/TAG", "Log statement description/message");
-	lnmEventPushS(event, lnmVeryVerbose, "INVOKER/TAG", "Log statement description/message");
-	lnmEventPushS(event, lnmWarning, "INVOKER/TAG", "Log statement description/message");
-	lnmEventPushS(event, lnmError, "INVOKER/TAG", "Log statement description/message");
 
-	lnm_debug_parse(event, 0);
+	printf("data testing\n");
+	printf("----------------------------\n");
+
+	printf("word size/ptr length: %li\n", sizeof(lnmItem));
+	printf("\n");
+
+	printf("enum lnmVerbosityLevel {\n");
+	printf("\tlnmInfo = %d,\n\tlnmDebug = %d,\n\tlnmVerbose = %d,\n\tlnmVeryVerbose = %d,\n\tlnmWarning = %d,\n\tlnmError = %d\n}\n", lnmInfo, lnmDebug, lnmVerbose, lnmVeryVerbose, lnmWarning, lnmError);
+	printf("\n\n");
+
+	printf("core library\n");
+	printf("----------------------------\n");
+
+	printf("creating \"%s\" queue with path \"%s\"\n", queueName, queuePath);
+	lnmQueue queue = lnmQueueInit(queueName, queuePath);
+	printf("queue \"%s\" created at 0x%lx\n", queueName, (long)queue);
+	printf("\n");
+
+	printf("checking queue integrity in registry...\n");
+	printf("queue == lnmQueueByName(\"%s\"): ", queueName);
+	if (queue == lnmQueueByName(queueName)) {
+		printf("true\n");
+	} else {
+		printf("false. exiting...\n");
+		return 1;
+	}
+	printf("\n");
+
+	printf("creating an E{S} logtree\n");
+	lnmEventS(lnmError, "Tag / Invoker", "Test statement pushed to single event with custom tag and message");
+	printf("\n");
+
+	printf("debug registry logtree (1 top level item expected)\n");
+	lnm_debug_parse_registry();
+	printf("\n\n");
+
+	printf("tests finished\n");
+	printf("----------------------------\n");
+	printf("time elapsed (us): %li\n", lnm_getus() - t1);
 
 	return 0;
 }
