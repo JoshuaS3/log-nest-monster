955bc8d4b449e84d441417f70513c18f77e666a8
Josh Stockin <joshstockin@gmail.com>
2019-12-29 19:58:46 -0600

    Change license from GPLv3 to MIT
    Allows more end user freedom regarding binaries and proprietary
    codebases.  Hopefully makes the library more inviting to others and
    expands potential coverage.

--- src/c/lognestmonster.h
+++ src/c/lognestmonster.h
@@ -1,24 +1,9 @@
-// lognestmonster Copyright (c) 2019 Joshua 'joshuas3' Stockin
+// lognestmonster Copyright (c) 2020 Joshua 'joshuas3' Stockin (copyrighted under the MIT License)
+// <https://joshstock.in>
+// <https://github.com/JoshuaS3/lognestmonster>
+//
 // lognestmonster.h
 // C header file for implementation of the lognestmonster logging library
-
-// <https://github.com/JoshuaS3/lognestmonster/>.
-
-
-// This file is part of lognestmonster.
-
-// lognestmonster is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-
-// lognestmonster is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
-
-// You should have received a copy of the GNU General Public License
-// along with lognestmonster. If not, see <https://www.gnu.org/licenses/>.
 
 
 // SEMANTICS
