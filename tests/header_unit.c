0ab0f7169579af72e37096c365e67ac2fc682889
Josh Stockin <joshstockin@gmail.com>
2020-02-06 18:39:33 -0600

    Bring header unit tests up to date

--- tests/header_unit.c
+++ tests/header_unit.c
@@ -1,8 +1,14 @@
-// lognestmonster Copyright (c) 2020 Joshua 'joshuas3' Stockin
-// <https://joshstock.in>
-// <https://github.com/JoshuaS3/lognestmonster>
-// This software is licensed under the MIT License.
-//
+/* lognestmonster Copyright (c) 2020 Joshua 'joshuas3' Stockin
+ * <https://joshstock.in>
+ * <https://github.com/JoshuaS3/lognestmonster>
+ *
+ * This software is licensed and distributed under the terms of the MIT License.
+ * See the MIT License in the LICENSE file of this project's root folder.
+ *
+ * This comment block and its contents, including this disclaimer, MUST be
+ * preserved in all copies or distributions of this software's source.
+ */
+
 // main.c
 // C file for testing the lognestmonster library header
 
@@ -12,89 +18,68 @@
 #define LNM_DEBUG
 #include "lognestmonster.h"
 
-void test(void);
-extern char * queueName;
-static char * queuePath = "~/path";
+void verify_queue_integrity(lnmQueue queue, char * queueName);
 
 int main(void) {
-	queueName = "master";
 	long t1 = lnm_getus();
 
-	printf("lognestmonster C test main()\n");
-	printf("============================\n");
-	printf("\n\n");
-
-
 	printf("data testing\n");
-	printf("----------------------------\n");
+	printf("============\n");
 
-	printf("word size/ptr length: %lu\n", sizeof(lnmItem));
-	printf("\n");
-
-	printf("enum lnmVerbosityLevel {\n");
-	printf("\tlnmInfo = %i,\n\tlnmDebug = %i,\n\tlnmVerbose = %i,\n\tlnmVeryVerbose = %i,\n\tlnmWarning = %i,\n\tlnmError = %i\n}\n", lnmInfo, lnmDebug, lnmVerbose, lnmVeryVerbose, lnmWarning, lnmError);
+	printf("word size/ptr length: %zu\n", sizeof(lnmItem));
 	printf("\n\n");
 
-	printf("core library\n");
-	printf("----------------------------\n");
+	printf("queue persistence testing\n");
+	printf("=========================\n");
 
-	printf("creating \"%s\" queue with path \"%s\"\n", queueName, queuePath);
-	lnmQueue queue = lnmQueueInit(queueName, queuePath);
-	printf("queue \"%s\" created at 0x%llx\n", queueName, (long long)queue);
+	printf("creating \"master\" queue with path \"/var/log/lognestmonster\"\n");
+	lnmQueue queue = lnmQueueInit("master", "/var/log/lognestmonster");
+	printf("queue \"master\" created at 0x%llx\n", (long long)queue);
 	printf("\n");
 
 	printf("checking queue integrity in registry...\n");
-	printf("queue == lnmQueueByName(\"%s\"): ", queueName);
-	if (queue == lnmQueueByName(queueName)) {
+	printf("tests/header_unit.c: queue == lnmQueueByName(\"master\"): ");
+	if (queue == lnmQueueByName("master")) {
 		printf("true\n");
 	} else {
 		printf("false. exiting...\n");
 		return 1;
 	}
-	printf("\n");
+	printf("checking queue integrity across translation units...\n");
+	verify_queue_integrity(queue, "master");
+	printf("\n\n");
 
-	printf("creating an E{2S, E{1S}, 1S} logtree\n");
-	lnmItem event = lnmEventS("event_tag", lnmError, "INVOKER", "Test ERROR statement pushed to single event with custom tag and message");
-	lnmEventPushS(event, lnmInfo, "INIT", "Sample INFO/INIT log statement");
+	printf("log tree persistence testing\n");
+	printf("============================\n");
 
-	lnmItem event2 = lnmEventS("event 2", lnmVerbose, "NESTED", "Example of a nested log statement");
-	lnmEventPushS(event2, lnmVeryVerbose, "NESTED", "Nested #2");
-
-	lnmEventPush(event, event2);
-
-	lnmEventPushS(event2, lnmInfo, "TEST", "Item #3");
-	lnmEventPushS(event2, lnmInfo, "TEST", "Item #4");
-
-	lnmItem event3 = lnmEventS("asdf", lnmWarning, "NESTED-2", "Third layer log statement");
-	lnmEventPush(event2, event3);
-
-	lnmEventPushS(event2, lnmInfo, "TEST", "Item #6");
-	lnmEventPushS(event2, lnmInfo, "TEST", "Item #7");
-	lnmEventPushS(event2, lnmInfo, "TEST", "Item #8");
-	lnmEventPushS(event2, lnmInfo, "TEST", "Item #9 (frame capacity doubles from 8 to 16)");
-
-	lnmEventPushS(event, lnmDebug, "REQUEST", "DEBUG/REQUEST log statement. might be found useful in a webserver backend");
-	printf("\n");
+	printf("creating log tree\n");
+	lnmItem event = lnmEvent("EVENT_TAG");
+	lnmEventPushS(event, lnmInfo, "Each log statement has a verbosity level attached and holds general information you want to write in a log file.");
+	lnmEventPushS(event, lnmVerbose, "There are 6 verbosity levels: lnmInfo, lnmDebug, lnmVerbose, lnmVeryVerbose, lnmWarning, and lnmError.");
+	lnmEventPushS(event, lnmInfo, "An event is a container for log statements or other events. Events can later be deserialized and queried.");
+	lnmEventPushS(event, lnmVeryVerbose, "Events can hold other events, like this: ");
+	lnmItem nested_event = lnmEvent("NESTED_EVENT");
+	lnmEventPush(event, nested_event);
+	lnmEventPushS(nested_event, lnmDebug, "Each event is assigned a tag to make querying easy.");
+	lnmEventPushS(nested_event, lnmWarning, "Events can hold up to 2^16 (65536) items. Messages also have a 2^16 maximum message length.");
+	printf("created log tree\n");
+	printf("\n\n");
 
 	printf("debug registry logtree (1 top level items expected)\n");
 	lnm_debug_parse_registry();
-	printf("\n");
+	printf("\n\n");
 
 	printf("push event to queue\n");
 	lnmQueuePush(queue, event);
-	printf("\n");
-
-	printf("debug queue (master)\n");
-	lnm_debug_parse_queue(queue);
 	printf("\n");
 
 	printf("debug registry logtree (0 top level items expected)\n");
 	lnm_debug_parse_registry();
 	printf("\n");
 
-	printf("calling test function from different source file\n");
-	test();
-	printf("\n");
+	printf("debug queue (master)\n");
+	lnm_debug_parse_queue(queue);
+	printf("\n\n");
 
 	printf("freeing queue\n");
 	lnm_free_queue(queue);
--- tests/header_unit_2.c
+++ tests/header_unit_2.c
@@ -1,16 +1,28 @@
-// lognestmonster Copyright (c) 2020 Joshua 'joshuas3' Stockin
-// <https://joshstock.in>
-// <https://github.com/JoshuaS3/lognestmonster>
-// This software is licensed under the MIT License.
-//
-// main_test.c
-// Second part of a two-piece lognestmonster unit test source
+/* lognestmonster Copyright (c) 2020 Joshua 'joshuas3' Stockin
+ * <https://joshstock.in>
+ * <https://github.com/JoshuaS3/lognestmonster>
+ *
+ * This software is licensed and distributed under the terms of the MIT License.
+ * See the MIT License in the LICENSE file of this project's root folder.
+ *
+ * This comment block and its contents, including this disclaimer, MUST be
+ * preserved in all copies or distributions of this software's source.
+ */
 
-#include <stdio.h>
+// main_test.c
+// Second part of the main lognestmonster unit test. Tests queue integrity
+// across translation units.
+
+#include <stdio.h>   // printf
+#include <stdlib.h>  // abort
 #include "lognestmonster.h"
 
-char * queueName;
-void test(void) {
-	printf("retrieving %s from global queue\n", queueName);
-	lnmQueueByName(queueName);
+void verify_queue_integrity(lnmQueue queue, char * queueName) {
+	printf("tests/header_unit_2.c: queue == lnmQueueByName(\"%s\"): ", queueName);
+	if (queue == lnmQueueByName(queueName)) {
+		printf("true\n");
+	} else {
+		printf("false. exiting...\n");
+		abort();
+	}
 }
